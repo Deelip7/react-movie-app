06ea53be1cebbe182c22c23a2169bc286ed3cd90
Theelipan prabakar <theelipan@gmail.com>
2020-10-08 21:02:47 -0400

    install scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Movie APP</h1>
+      <header>
+        <h1>Movie APP</h1>
+      </header>
+      <main></main>
     </div>
   );
 }
